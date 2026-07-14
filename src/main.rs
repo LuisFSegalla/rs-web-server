941254d3c7e4f0026f39c9b7783e9e47ca8be778
LuisFSegalla <luisfsegalla@gmail.com>
2026-07-14 22:17:26 +0000

    First commit
    Project used as a way to study Rust.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,92 @@
-use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
 use tera::{Context, Tera};
 use std::sync::Mutex;
 mod server;
 
 
 #[get("/data")]
-async fn index(tera: web::Data<Tera>, data: web::Data<server::UserNameProtected>) -> impl Responder {
-    let  read_data: std::sync::MutexGuard<'_, String> = data.name.lock().unwrap();
+async fn index(tera: web::Data<Tera>, data: web::Data<server::UserDataProtected>) -> impl Responder {
+    let  read_data: std::sync::MutexGuard<'_, server::UserData> = data.data.lock().unwrap();
     let mut ctx: Context = Context::new();
-    ctx.insert("name", &read_data.clone()); //Not great that I have to clone the value that is shared here
+    ctx.insert("name", &read_data.name.clone());
     HttpResponse::Ok().body(tera.render("base.html", &ctx).unwrap())
 }
 
-async fn set_val(path: web::Path<String>, data: web::Data<server::UserNameProtected>) -> String {
+#[get("/zmq_data")]
+async fn zmq_data(tera: web::Data<Tera>, data: web::Data<server::UserDataProtected>) -> impl Responder {
+    let _ = server::get_zmq_data(&data).await;
+    let  read_data: std::sync::MutexGuard<'_, server::UserData> = data.data.lock().unwrap();
+    let mut ctx: Context = Context::new();
+    ctx.insert("name", &*read_data.name); 
+    ctx.insert("points", &*read_data.data);
+    HttpResponse::Ok().body(tera.render("array.html", &ctx).unwrap())
+}
+
+#[get("/multi_data")]
+async fn zmq_multi_data(tera: web::Data<Tera>, data: web::Data<server::UserDataProtected>) -> impl Responder {
+    let _ = server::get_zmq_data(&data).await;
+    let  read_data: std::sync::MutexGuard<'_, server::UserData> = data.data.lock().unwrap();
+    let mut ctx: Context = Context::new();
+    ctx.insert("points", &*read_data.data);
+    ctx.insert("name", &*read_data.name);
+    ctx.insert("windows", &8);
+    ctx.insert("values_per_window", &4096);
+
+    HttpResponse::Ok().body(tera.render("multi_window.html", &ctx).unwrap())
+}
+
+
+#[get("/rand_data")]
+async fn rand_data(tera: web::Data<Tera>, data: web::Data<server::UserDataProtected>) -> impl Responder {
+    let _ = server::server("127.0.0.1:6767".to_string(), &data).await;
+    let  read_data: std::sync::MutexGuard<'_, server::UserData> = data.data.lock().unwrap();
+    let mut ctx: Context = Context::new();
+    ctx.insert("name", &read_data.name.clone());
+    HttpResponse::Ok().body(tera.render("base.html", &ctx).unwrap())
+}
+
+#[get("/demo")]
+async fn demo(tera: web::Data<Tera>) -> impl Responder {
+    let ctx: Context = Context::new();
+    HttpResponse::Ok().body(tera.render("demo.html", &ctx).unwrap())
+}
+
+async fn set_val(path: web::Path<String>, data: web::Data<server::UserDataProtected>) -> impl Responder{
     let val: String = path.into_inner();
-    let mut _data = data.name.lock().unwrap();
-    *_data = val;
-    format!("new data = {_data}")    
+    let mut _data: std::sync::MutexGuard<'_, server::UserData> = data.data.lock().unwrap();
+    _data.name = val;
+    ""
 }
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut data: server::UserNameProtected = server::UserNameProtected{
-        name: Mutex::new("NoName".to_string())
-    };    
-    let web_data: web::Data<server::UserNameProtected> = web::Data::new(data);
+    let data: server::UserDataProtected = server::UserDataProtected{
+        data: Mutex::new(
+            server::UserData{
+                name: "NoName".to_string(),
+                id: 0,
+                data: [0.0, 1.0, 2.0].to_vec()
+            }
+        )
+    };
+    let web_data: web::Data<server::UserDataProtected> = web::Data::new(data);
 
     let mut tera = Tera::new();
     tera.add_template_file("src/templates/base.html", Some("base.html")).unwrap();
+    tera.add_template_file("src/templates/array.html", Some("array.html")).unwrap();
+    tera.add_template_file("src/templates/multi_window.html", Some("multi_window.html")).unwrap();
+    tera.add_template_file("src/templates/demo.html", Some("demo.html")).unwrap();
 
     HttpServer::new(move || {
         App::new()
         .app_data(web_data.clone())
         .app_data(web::Data::new(tera.clone()))
         .service(index)
+        .service(rand_data)
+        .service(zmq_data)
+        .service(zmq_multi_data)
+        .service(demo)
         .route("/{name}", web::get().to(set_val))
     })
     .bind("127.0.0.1:8080")?
--- src/server.rs
+++ src/server.rs
@@ -2,12 +2,23 @@ use zeromq::{Socket, SocketRecv};
 use std::error::Error;
 use std::sync::Mutex;
 use rand::{RngExt};
+use actix_web::web;
+use serde::{Serialize, Deserialize};
 
-pub struct UserNameProtected {
-    pub name: Mutex<String>,
+pub struct UserDataProtected {
+    pub data: Mutex<UserData>,
 }
 
-pub async fn server(url: String, data: UserNameProtected) -> Result<(), Box<dyn Error>> {
+# [derive(Serialize,Deserialize, Debug)]
+pub struct UserData {
+    pub name: String,
+    pub id: i32,
+    pub shape: (i32,i32),
+    pub data: Vec<f64>
+
+}
+
+pub async fn server(url: String, data: &web::Data<UserDataProtected>) -> Result<(), Box<dyn Error>> {
     // let mut socket: zeromq::SubSocket = zeromq::SubSocket::new();
     // socket.connect(&url).await?;
     // socket.subscribe("").await?;
@@ -16,11 +27,28 @@ pub async fn server(url: String, data: UserNameProtected) -> Result<(), Box<dyn 
     
     loop {
         // let repl: String = socket.recv().await?.try_into()?;
-        let mut _data = data.name.lock().unwrap();
+        let mut _data = data.data.lock().unwrap();
         // *_data = repl;
         let mut rng = rand::rng();
         let n: u32 = rng.random_range(0..100);
-        *_data = n.to_string();
-        println!("*_data = {_data}");
+        _data.id = rng.random_range(0..100);
+        _data.name = n.to_string();
+        // *_data.data = 
     }
+}
+
+
+pub async fn get_zmq_data(data: &web::Data<UserDataProtected>) -> Result<(), Box<dyn Error>>{
+    let mut socket: zeromq::SubSocket = zeromq::SubSocket::new();
+    socket.connect(&"tcp://127.0.0.1:8081".to_string()).await?;
+    socket.subscribe("").await?;
+    println!("connected to 127.0.0.1:8081");
+    let repl: String = socket.recv().await?.try_into()?;
+    let json: UserData = serde_json::from_str(&repl)?;
+    let mut _data= data.data.lock().unwrap(); // <- get counter's MutexGuard
+    _data.id = json.id;
+    _data.name = json.name;
+    _data.data = json.data;
+    _data.shape = json.shape;
+    Ok(())
 }
