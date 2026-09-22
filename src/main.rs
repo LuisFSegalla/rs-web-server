d0047c3f008f49580f197286c6ee1e1a14368a2a
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2026-09-22 13:36:49 +0100

    Add logic to enable configuration of server IPs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, get, web};
 use actix_web_actors::ws;
 use tera::{Context, Tera};
-// use std::sync::{Arc, Mutex};
 use tokio::sync::{broadcast};
-
+use clap::Parser;
 use crate::server::UserData;
 
 mod server;
 
+/// web server application for live viewing of MCA data
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Address of the live view server to get data from
+    #[arg(short, long, default_value_t="tcp://127.0.0.1:15510".to_owned())]
+    live_server: String,
+
+    /// Address to host webserver
+    #[arg(short, long,default_value_t="127.0.0.1:8080".to_owned())]
+    web_server: String,
+}
 
 #[get("/data")]
 async fn index(tera: web::Data<Tera>, data: web::Data<broadcast::Sender<UserData>>) -> impl Responder {
@@ -54,9 +65,12 @@ async fn live(tera: web::Data<Tera>) -> HttpResponse {
 
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(), std::io::Error> {
     env_logger::init();
- 
+
+    let args: Args = Args::parse();
+    let live_view_addr: String = args.live_server;
+    let web_server_addr: String = args.web_server;
     let (tx, _rx) = broadcast::channel::<server::UserData>(100);
     let tx: web::Data<broadcast::Sender<UserData>> = web::Data::new(tx.clone());
 
@@ -65,23 +79,32 @@ async fn main() -> std::io::Result<()> {
     let _ = tokio::spawn(async move 
     {
         server::server(
-            "tcp://xspress-01-live-view-merger:15510".to_string(),
-            thread_data).await;
+            live_view_addr,
+            thread_data
+            ).await;
     });
 
     let mut tera = Tera::new();
     tera.add_template_file("src/templates/multi_window.html", Some("multi_window.html")).unwrap();
     tera.add_template_file("src/templates/live_data.html", Some("live_data.html")).unwrap();
 
-    HttpServer::new(move || {
+    let serv = HttpServer::new(move || {
         App::new()
         .app_data(tx.clone())
         .app_data(web::Data::new(tera.clone()))
         .route("/ws", web::get().to(ws))
         .service(index)
         .service(live)
-    })
-    .bind("0.0.0.0:1307")?
-    .run()
-    .await
+    });
+    match serv.bind(&web_server_addr) {
+        Ok(server) => {
+            log::info!("Connected web server to {web_server_addr}");
+            server.run().await
+        },
+        Err(e) => {
+            log::error!("Failed to connect to {web_server_addr}: {e}");
+            Err(e)
+        }
+    }
+
 }
